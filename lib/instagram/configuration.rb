6c6981fc97e65ba5de24905658cc2a629eec41a9
Shayne Sweeney <shayne@instagr.am>
2011-02-04 16:53:32 -0800

    Updated endpoint to new URL and bump to 0.5.0

--- lib/instagram/configuration.rb
+++ lib/instagram/configuration.rb
@@ -42,12 +42,12 @@ module Instagram
     # The endpoint that will be used to connect if none is set
     #
     # @note There is no reason to use any other endpoint at this time
-    DEFAULT_ENDPOINT = 'http://api-privatebeta.instagr.am/v1/'.freeze
+    DEFAULT_ENDPOINT = 'https://api.instagram.com/v1/'.freeze
     
     # The response format appended to the path and sent in the 'Accept' header if none is set
     #
     # @note JSON is the only available format at this time
-    DEFAULT_FORMAT = :json
+    DEFAULT_FORMAT = :json 
     
     # By default, don't use a proxy server
     DEFAULT_PROXY = nil
