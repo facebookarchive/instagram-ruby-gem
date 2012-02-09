b84aee3eb359b90692c7149f22caa2934b47a59f
Peter Bakkum <pbb7c@virginia.edu>
2012-02-09 12:17:10 -0500

    updated faraday version and fixed related issues

---
+++ lib/faraday/raise_http_exception.rb
@@ -0,0 +1,45 @@
+require 'faraday'
+
+# @private
+module FaradayMiddleware
+  # @private
+  class RaiseHttpException < Faraday::Middleware
+    def call(env)
+      @app.call(env).on_complete do |response|
+        case response[:status].to_i
+        when 400
+          raise Instagram::BadRequest, error_message_400(response)
+        when 404
+          raise Instagram::NotFound, error_message_400(response)
+        when 500
+          raise Instagram::InternalServerError, error_message_500(response, "Something is technically wrong.")
+        when 503
+          raise Instagram::ServiceUnavailable, error_message_500(response, "Instagram is rate limiting your requests.")
+        end
+      end
+    end
+
+    def initialize(app)
+      super app
+      @parser = nil
+    end
+
+    private
+
+    def error_message_400(response)
+      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(response[:body])}"
+    end
+
+    def error_body(body)
+      if body.nil?
+        nil
+      elsif body['meta'] and body['meta']['error_message'] and not body['meta']['error_message'].empty?
+        ": #{body['meta']['error_message']}"
+      end
+    end
+
+    def error_message_500(response, body=nil)
+      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{[response[:status].to_s + ':', body].compact.join(' ')}"
+    end
+  end
+end
