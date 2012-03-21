d4672cb46d598cf9559da11331dd0c5aea90547b
Jason <jasonm23@gmail.com>
2012-03-21 17:15:21 +1100

    modified Client.Users.user_recent_media to return `response` instead of `response["data"]`

--- lib/instagram/client/users.rb
+++ lib/instagram/client/users.rb
@@ -160,7 +160,7 @@ module Instagram
       options = args.last.is_a?(Hash) ? args.pop : {}
       id = args.first || "self"
       response = get("users/#{id}/media/recent", options)
-      response["data"]
+      response
     end
 
     # Returns a list of media items liked by the current user
