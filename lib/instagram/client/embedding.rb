6fb7d65743abc023ee08ea6bf59ed92a4dd77ea7
Petri Partio <petri.partio@gmail.com>
2014-09-12 18:51:25 +0300

    Change tabs to 2 spaces

--- lib/instagram/client/embedding.rb
+++ lib/instagram/client/embedding.rb
@@ -1,18 +1,18 @@
 module Instagram
   class Client
-  	# Defines methods related to embedding
+    # Defines methods related to embedding
     module Embedding
-    	# Returns information about the media associated with the given short link
+      # Returns information about the media associated with the given short link
       #
-	    # @overload oembed(url=nil, options={})
-	    # 	@param url [String] An instagram short link
-	    #   @param options [Hash] A customizable set of options
-	    #   @option options [Integer] :maxheight Maximum height of returned media
-	    #   @option options [Integer] :maxwidth Maximum width of returned media
-	    #   @option options [Integer] :callback A JSON callback to be invoked
-	    #   @return [Hashie::Mash] Information about the media associated with given short link
-      # 	@example Return information about the media associated with http://instagr.am/p/BUG/
-      #   	Instagram.oembed(http://instagr.am/p/BUG/)
+      # @overload oembed(url=nil, options={})
+      #   @param url [String] An instagram short link
+      #   @param options [Hash] A customizable set of options
+      #   @option options [Integer] :maxheight Maximum height of returned media
+      #   @option options [Integer] :maxwidth Maximum width of returned media
+      #   @option options [Integer] :callback A JSON callback to be invoked
+      #   @return [Hashie::Mash] Information about the media associated with given short link
+      #   @example Return information about the media associated with http://instagr.am/p/BUG/
+      #     Instagram.oembed(http://instagr.am/p/BUG/)
       #
       # @see http://instagram.com/developer/embedding/#oembed
       # @format :json
